3cbad6675346e0c3e6e82164550c8fe1a02cd8a8
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-09-11 17:56:37 -0400

    Update 02_tensors.py

--- 02_tensors.py
+++ 02_tensors.py
@@ -5,5 +5,21 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 
-x = tf.constant(4, shape=(1,1), dtype=tf.float32)
-print(x)
+# x = tf.constant(4, shape=(1,1), dtype=tf.float32)
+# print(x)
+# y= tf.random.normal((3,3), mean=0, stddev=1)
+# print(y)
+
+# z = tf.range(10)
+# print(z)
+
+# # cast
+# a = tf.cast(z, dtype=tf.float16)
+# print(a)
+
+#element wise operations 
+x = tf.constant([1,2,3])
+y = tf.constant([9,8,7])
+
+z = tf.add(x,y)
+print(z)
